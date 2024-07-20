0fa107fd7cd9ab9229a43c013dcb3ac311862cbd
moonbak <thomas.iw@kata.games>
2024-07-20 12:47:05 +0200

    bugfixes +websocket support added to py-client

--- py-client/tictactoe.py
+++ py-client/tictactoe.py
@@ -1,11 +1,10 @@
 import sys
-
 import pygame
-
 import glvars
 from GameModel import GameModel, EMPTY_CELL
 from core.events import EvManager, EngineEvTypes, EvListener
-from networking import NetwPusher
+import importlib
+import json
 
 
 pygame.mixer.pre_init(44100, -16, 2, 1024)
@@ -27,6 +26,23 @@ mouse = None
 selection = [None, None]
 
 
+# --------------
+#  load networking module
+# --------------
+with open('client-config.json') as fptr:
+    jobj = json.load(fptr)
+    target_host, transport = jobj['host'], jobj['transport_type']  # you can select either 'ws' or 'socket' for 2nd
+
+# Dynamically load the appropriate module
+networking_module = None
+if transport == 'socket':
+    networking_module = importlib.import_module('networking')
+elif transport == 'ws':
+    networking_module = importlib.import_module('ws_networking')
+else:
+    raise ValueError("Unsupported transport type")
+
+
 class GameGodObject(EvListener):
     def on_netw_receive(self, ev):
         global game_model_obj
@@ -40,11 +56,16 @@ class GameGodObject(EvListener):
         else:
             self.pev(glvars.gevents.ActivePlayerChanges)
 
+    def on_game_ends(self, ev):
+        pygame.time.wait(500)
+        print('»» Sending msg after won is True')
+        self.pev(EngineEvTypes.NetwSend, serial=game_model_obj.serialize())
+
     def on_update(self, ev):
         # TODO use events board cange to handle the logic
         global running, mouse, selection
         global game_model_obj
-        won = None
+
         mouse = pygame.mouse.get_pos()
         row, col = int(mouse[0] / 100), int(mouse[1] / 100)
         selection[0] = row
@@ -54,31 +75,27 @@ class GameGodObject(EvListener):
                 running = False
                 self.pev(glvars.gevents.ExitNetwork)
 
-            elif GameModel.test_full_board(game_model_obj.board):
-                # TODO why have we replaced this?
-                # model.reset_board()
-                game_model_obj.reset_game()
-
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if row < 3 and col < 3 and game_model_obj.board[row][col] == EMPTY_CELL:
                     curr_pl = game_model_obj.curr_player
                     if curr_pl == local_pl:
                         game_model_obj.play_move(row, col)
+
                         won = GameModel.test_winner(game_model_obj.board, curr_pl)
                         if won:
                             self.pev(glvars.gevents.GameEnds, soundname='resetSound.wav')
                             game_model_obj.score[curr_pl] += 1
-
-                        if not won:  # if won, we will push infos later, without increm turn
+                        else:
+                            if GameModel.test_full_board(game_model_obj.board):
+                                # TODO why have we replaced this?
+                                # model.reset_board()
+                                game_model_obj.reset_game()
+                                return
                             game_model_obj.next_turn()
                             self.pev(EngineEvTypes.NetwSend, serial=game_model_obj.serialize())
 
                 elif 250 < mouse[0] < 282 and 310 < mouse[1] < 342:
                     game_model_obj.reset_game()
-        if won:
-            pygame.time.wait(500)
-            print('»» Sending msg after won is True')
-            self.pev(EngineEvTypes.NetwSend, serial=game_model_obj.serialize())
 
 
 class GameView(EvListener):
@@ -178,7 +195,8 @@ mode_de_jeu = 0  # vs remote player
 pygame.mouse.set_pos(150, 175)
 ev_mger.post(glvars.gevents.ActivePlayerChanges)
 #if mode_de_jeu == 0:
-netw_pusher = NetwPusher()
+
+netw_pusher = networking_module.NetwPusher()
 netw_pusher.turn_on()
 #else:
 #    pass
---
+++ py-client/ws_networking.py
@@ -0,0 +1,69 @@
+import threading
+import websocket
+from core.events import EngineEvTypes, EvManager, EvListener
+
+client_socket = None
+receiver_thread = None
+
+
+class NetwPusher(EvListener):
+    def on_netw_send(self, ev):
+        send_data(ev.serial.encode())
+
+    def on_netw_exit(self, ev):
+        stop_network()
+
+    def turn_on(self):
+        super().turn_on()
+        start_client()
+
+
+def on_message(ws, message):
+    ev_manager = EvManager.instance()
+    serial = message
+    print(f'Received shared variable update: {serial}')
+    ev_manager.post(EngineEvTypes.NetwReceive, serial=serial)
+
+
+def on_error(ws, error):
+    print(f"WebSocket error: {error}")
+
+
+def on_close(ws, close_status_code, close_msg):
+    print("WebSocket connection closed")
+
+
+def on_open(ws):
+    global client_socket
+    client_socket = ws
+    print("WebSocket connection opened")
+
+
+def receive_updates():
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:8080/artificial.js",
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+    ws.run_forever()
+
+
+def start_client():
+    global receiver_thread
+    receiver_thread = threading.Thread(target=receive_updates)
+    receiver_thread.start()
+
+
+def stop_network():
+    global receiver_thread, client_socket
+    if receiver_thread:
+        receiver_thread.join()
+    if client_socket:
+        client_socket.close()
+
+
+def send_data(x):
+    global client_socket
+    if client_socket:
+        client_socket.send(x)
