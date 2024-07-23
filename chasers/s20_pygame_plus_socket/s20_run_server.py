c4132ad7f2cb172eac4799582e8ee1ccb7e996c7
moonbak <thomas.iw@kata.games>
2024-07-23 17:58:09 +0200

    [demo s20]version server-side logic+AI is now moving

--- chasers/s20_pygame_plus_socket/s20_run_server.py
+++ chasers/s20_pygame_plus_socket/s20_run_server.py
@@ -1,4 +1,4 @@
-from ServerMediator import ServerMediator
+from UMediator import UMediator
 from ServerComponent import ServerComponent
 from NetworkLayer2 import NetworkLayer2
 import time
@@ -9,11 +9,13 @@ PORT = 60111
 
 if __name__ == '__main__':
     netl = NetworkLayer2()
-    serv_side_mediator = ServerMediator(netl)
+    serv_side_mediator = UMediator(True, netl)
     # je crois qu'il faut attendre de s'etre register sur netlayer avant de start comms
     # sinon liste des mediators est vide
     netl.start_comms((HOST, PORT))
     serv_obj = ServerComponent(serv_side_mediator)
     while True:
+        serv_obj.proc_server_logic(time.time())
+
         serv_obj.mediator.update()
         time.sleep(0.1)
